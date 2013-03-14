7a6425481654e97de02a400e85c85d6c4f22cd12
kahonyuun <kahonyuun@gmail.com>
2013-03-14 18:50:18 +0900

    TODO : int型になおす at chart_test.js 55

--- Resources/chart_test.js
+++ Resources/chart_test.js
@@ -33,35 +33,50 @@ function Highcharts(nav) {
 	rows.closed
 	db.close();
 
-	//TODO in_timeを月日に分割する
+	//TODO チャートのためにin_timeを月日に分割する
 	//TODO 早急にリファクタリング
-	// var db = Ti.Database.open('db');
-	// var rows = db.execute('select rowid, * from date_test');
-// 
-	// var contain_days = [];
-	// while (rows.isValidRow()) {
-		// var rowid = rows.fieldByName('rowid');
-		// var in_time = rows.fieldByName('in_time');
+	var db = Ti.Database.open('db');
+	var rows = db.execute('select rowid, * from date_test');
+
+	var contain_days = [];
+	while (rows.isValidRow()) {
+		var rowid = rows.fieldByName('rowid');
+		var in_time_string = rows.fieldByName('in_time');
 		//Ti.API.info("in_time" + in_time);
-		//contain_days[rowid] = in_time;
-// 
-		// rows.next();
-	// }
-	// rows.closed
-	// db.close();
+
+		in_reg = /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/g;
+		//文字列から変換
+		dateArray = in_reg.exec(in_time_string)
+		var in_time_date = new Date((+dateArray[1]), //年
+		(+dateArray[2]) - 1, // Careful, month starts at 0!
+		(+dateArray[3]));
+		
+		Ti.API.info("in_time_date :" + in_time_date);
+
+		//TODO これなにしてるんだろうか
+		var in_time_timestamp = in_time_date.getTime();
+
+		var sleep_days = dateArray[2] + "/" + dateArray[3];
+		Ti.API.info("sleep_days: "+sleep_days);
+
+		contain_days[rowid] = sleep_days;
+		//Ti.API.info(contain_days);
+		rows.next();
+	}
+	rows.closed
+	db.close();
 
 	//②睡眠時間to chart_test.htmlへ配達
 	webView.addEventListener('load', function(e) {
 		var hours = chart_contain;
 
-	
 		//Ti.API.info(hours);
 		//Ti.API.info(hours[0]);
 		//Ti.API.info(hours[1]);
 
 		//webView.evalJS("chart( HOUR = " + hours + ")");//でないTODO ブログ
 		webView.evalJS("chart( HOUR = " + JSON.stringify(hours) + " )");
-		//webView.evalJS("chart( DAY = " + JSON.stringify(in_time) + ")");
+		webView.evalJS("chart( DAY = " + JSON.stringify(sleep_days) + ")");
 
 		//webView.evalJS("viewChart(TITLE = '睡眠時間',TITLE2 = '睡眠時間2')");
 	});
