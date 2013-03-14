8544e09adbf5fc142a735dc8b31bdb1763d9a14a
kahonyuun <kahonyuun@gmail.com>
2013-03-14 15:08:19 +0900

    DBいじる前に動いてた記録

--- Resources/chart_test.js
+++ Resources/chart_test.js
@@ -13,37 +13,38 @@ function Highcharts(nav) {
 		url : '/etc/graph/highcharts/chart_test.html'//highcharts
 		// url で指定したファイルに変数をわたしたい
 		// param: total_sleep_array,
-
 	});
-	
-	//チャートに表示する時間の取得
+
+	//チャートに表示する時間をDBから取得
 	var db = Ti.Database.open('db');
 	var rows = db.execute('select rowid, * from date_test');
 
-var chart_contain = [];
+	var chart_contain = [];
 	while (rows.isValidRow()) {
-		var rowid =rows.fieldByName('rowid');
+		var rowid = rows.fieldByName('rowid');
 		var sleep_time = rows.fieldByName('sleep_time');
 		chart_contain[rowid] = sleep_time;
-		
-//Ti.API.info("睡眠時間"+rows.fieldByName('sleep_time'));
-//Ti.API.info(rows.fieldByName('rowid'));
-		rows.next();
 
+		//Ti.API.info("睡眠時間"+rows.fieldByName('sleep_time'));
+		//Ti.API.info(rows.fieldByName('rowid'));
+		rows.next();
 	}
 	rows.closed
 	db.close();
 
-	//以下柳田神
+	//睡眠時間をto chart_test.htmlへ配達
 	webView.addEventListener('load', function(e) {
-		var test = chart_contain;
-		Ti.API.info(test);//はい
-		//Ti.API.info(test[0]);
-		Ti.API.info(test[1]);
-		Ti.API.info(test[2]);
-		//out.jsのminuteがほしい
-		//webView.evalJS("chart( ID = " + test + ")");
-		webView.evalJS("chart( ID = " + JSON.stringify(test) + " )");
+		var days = contain_days;
+		var hours = chart_contain;
+
+		Ti.API.info(hours);
+		Ti.API.info(days);
+		//はい
+		//Ti.API.info(hours[0]);
+		//Ti.API.info(hours[1]);
+
+		//webView.evalJS("chart( HOUR = " + hours + ")");
+		webView.evalJS("chart( HOUR = " + JSON.stringify(hours) + " )");
 
 		//webView.evalJS("viewChart(TITLE = '睡眠時間',TITLE2 = '睡眠時間2')");
 	});
